a37e9bffb06d577a3f1ffd59a0d9ac7f82825b06
Haïder Alshamma <haideralsh@gmail.com>
2019-12-26 18:58:40 -0500

    Formatting

--- collections/Queue.ts
+++ collections/Queue.ts
@@ -1,7 +1,9 @@
 import Collection from "./Collection";
 
-class Queue<T> extends Collection<T> {
+export default class Queue<T> extends Collection<T> {
   add = (el: T): Array<T> => (this.collection = [...this.collection, el]);
+
   remove = (): Array<T> => (this.collection = [...this.collection.slice(1)]);
+
   peek = (): T => this.collection[0] ?? null;
 }
