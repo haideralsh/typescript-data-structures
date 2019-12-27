c5c965c487324d92fac10cfcc81b364258c642bc
Haïder Alshamma <haideralsh@gmail.com>
2019-12-27 01:23:42 -0500

    Add size to make the hashing algorithm work

--- HashTable.ts
+++ HashTable.ts
@@ -1,13 +1,17 @@
 export default class HashTable {
   private table = [];
+  size = 0;
+
+  constructor(size = 32) {
+    this.size = size;
+  }
 
   private hash = key =>
-    [...key].reduce((hash, char) => hash + char.charCodeAt(0), 0) %
-    this.table.length;
+    [...key].reduce((hash, char) => hash + char.charCodeAt(0), 0) % this.size;
 
   set = (key, value) => (this.table[this.hash(key)] = value);
 
-  get = key => this.table[this.hash(key)];
+  get = key => this.table[this.hash(key)] ?? null;
 
   delete = key => (this.table[this.hash(key)] = undefined);
 
